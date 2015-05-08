75345be66bfc044d6359010a7914c63dc80f0b2b
tusharmath <tusharmath@gmail.com>
2015-05-08 15:21:01 +0530

    --wip--

--- test/test.ioc.js
+++ test/test.ioc.js
@@ -1,207 +1,50 @@
-var Injector, expect, should;
+"use strict";
 
-should = require('chai').should();
+var should = require('chai').should();
 
-expect = require('chai').expect;
+var expect = require('chai').expect;
 
-Injector = require('../lib/ioc');
+var Container = require('../lib/ioc');
 
-describe('Injector', function () {
+describe('Container', function () {
+    class A {
+    }
+    class B extends A {
+    }
+    class C {
+    }
     beforeEach(function () {
-        this.annotate = Injector.annotate;
-        this.mod = new Injector;
-        this.A = function () {
-        };
-        return this.B = function () {
-        };
-    });
-    it('should exist', function () {
-        return should.exist(Injector);
-    });
-    describe('get()', function () {
-        it('returns an instance of a class', function () {
-            return this.mod.get(this.A).should.be.an.instanceOf(this.A);
-        });
-        it('returns an instance of a class with dependencies', function () {
-            var temp2, x;
-            temp2 = (function () {
-                function temp2(a1, b) {
-                    this.a = a1;
-                    this.b = b;
-                }
+        this.container = new Container()
+    })
+    it('throws as unregisterd', function () {
+        expect(function () {
+            this.container.resolve(A)
+        }.bind(this))
+            .to.Throw('class has not been registered')
+    })
 
-                return temp2;
+    it('resolves a class', function () {
+        this.container.register(A)
+        this.container.resolve(A).should.be.an.instanceOf(A)
+    })
 
-            })();
-            this.annotate(temp2).inject(this.A, this.B);
-            x = this.mod.get(temp2);
-            x.a.should.be.an.instanceOf(this.A);
-            return x.b.should.be.an.instanceOf(this.B);
-        });
-        return it('can not depend on objects', function () {
-            var A;
-            A = (function () {
-                function A() {
-                }
+    it('resolves with injections', function () {
+        class X {
+            constructor(a) {
+                this.a = a
+            }
+        }
+        this.container.register(A)
+        this.container.register(X).inject(A)
+        this.container.resolve(X).a.should.be.an.instanceOf(A)
+    })
+    it('resolves a singleton class', function () {
+        class X {
+        }
+        this.container.register(X).asSingleton()
+        var x1 = this.container.resolve(X),
+            x2 = this.container.resolve(X)
+        x1.should.equal(x2)
+    })
 
-                return A;
-
-            })();
-            this.annotate(A).inject({});
-            return expect((function (_this) {
-                return function () {
-                    return _this.mod.get(A);
-                };
-            })(this)).to["throw"]('Constructor expected');
-        });
-    });
-    describe('get().asSingleton()', function () {
-        it('returns the same instance of a class', function () {
-            this.annotate(this.A).asSingleton();
-            return this.mod.get(this.A).should.equal(this.mod.get(this.A));
-        });
-        return it('returns itself when asked for', function () {
-            return this.mod.get(Injector).should.equal(this.mod);
-        });
-    });
-    describe('get().extends()', function () {
-        it('supports prototypical inheritance', function () {
-            this.annotate(this.A)["extends"](this.B);
-            return this.mod.get(this.A).should.be.an.instanceOf(this.B);
-        });
-        it('supports prototypical inheritance', function () {
-            var A, B;
-            A = (function () {
-                function A() {
-                }
-
-                A.prototype.print = function () {
-                    return " World";
-                };
-
-                return A;
-
-            })();
-            B = (function () {
-                function B() {
-                }
-
-                B.prototype.print = function () {
-                    return "Hello" + B.__super__.print.apply(this, arguments);
-                };
-
-                return B;
-
-            })();
-            this.annotate(B)["extends"](A);
-            return this.mod.get(B).print().should.equal("Hello World");
-        });
-        return it('throws if base class is a singleton', function () {
-            var Base, Child1;
-            Base = (function () {
-                function Base() {
-                }
-
-                Base.prototype.print = function () {
-                    return "World";
-                };
-
-                return Base;
-
-            })();
-            Child1 = (function () {
-                function Child1() {
-                }
-
-                Child1.prototype.print = function () {
-                    return "Hello " + Child1.__super__.print.apply(this, arguments);
-                };
-
-                return Child1;
-
-            })();
-            this.annotate(Base).asSingleton();
-            this.annotate(Child1)["extends"](Base);
-            return expect((function (_this) {
-                return function () {
-                    return _this.mod.get(Child1);
-                };
-            })(this)).to["throw"]("can not instantiate if the class extends a singleton");
-        });
-    });
-    describe('get().resolveAs()', function () {
-        it('resolves with the resolver', function () {
-            var A, i;
-            i = 100;
-            A = (function () {
-                function A() {
-                    this.j = 0;
-                }
-
-                A.prototype.create = function () {
-                    return [i++, ++this.j];
-                };
-
-                return A;
-
-            })();
-            this.annotate(A).resolveAs(function (a) {
-                return a.create();
-            });
-            this.mod.get(A).should.deep.equal([100, 1]);
-            return this.mod.get(A).should.deep.equal([101, 1]);
-        });
-        it('resolves singletons with the resolver', function () {
-            var A, i;
-            i = 100;
-            A = (function () {
-                function A() {
-                    this.j = 0;
-                }
-
-                A.prototype.create = function () {
-                    return [++i, ++this.j];
-                };
-
-                return A;
-
-            })();
-            this.annotate(A).asSingleton().resolveAs(function (a) {
-                return a.create();
-            });
-            return this.mod.get(A).should.equal(this.mod.get(A));
-        });
-        return it('resolveAs callback is called with instance of ioc', function () {
-            var A, ioc;
-            A = function () {
-            };
-            ioc = null;
-            this.annotate(A).resolveAs(function (a, _ioc) {
-                return ioc = _ioc;
-            });
-            this.mod.get(A);
-            return ioc.should.equal(this.mod);
-        });
-    });
-    return describe("get().providerFor()", function () {
-        beforeEach(function () {
-            this.AMock = function () {
-                return this.jupiter = 200;
-            };
-            this.A = function () {
-                return this.jupiter = 100;
-            };
-            this.annotate(this.AMock).providerFor(this.A);
-            return this.mod = new Injector(this.AMock);
-        });
-        it("provides a mock instance", function () {
-            return (this.mod.get(this.A) instanceof this.AMock).should.be.ok;
-        });
-        return it("supports resolveAs", function () {
-            this.annotate(this.A).resolveAs(function (i) {
-                return i.jupiter;
-            });
-            return this.mod.get(this.A).should.equal(200);
-        });
-    });
 });
